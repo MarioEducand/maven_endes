0b071061e5bfe6cf801f93a368eae23ab8dc24b6
Mario Cano <mcanmig@g.educaand.es>
2026-05-05 19:54:34 +0200

    Cam

--- src/main/java/com/miempresa/app/Employee.java
+++ src/main/java/com/miempresa/app/Employee.java
@@ -1,5 +1,18 @@
 package com.miempresa.app;
 
+/**
+ * @author Mario Cano
+ * @version 1.0
+ */
 public class Employee {
+    private String name;
+    private int years;
 
+    public Employee(String name, int years) {
+        this.name = name;
+        this.years = years;
+    }
+
+    public String getName() { return name; }
+    public int getYears() { return years; }
 }
